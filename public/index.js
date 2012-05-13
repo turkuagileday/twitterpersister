241e8c6673d607cc782fbe2d4aea35c39a7fe4a4
Niklas Närhinen <niklas.narhinen@ecom.fi>
2012-05-13 18:34:26 +0300

    Add links to results

---
+++ public/index.js
@@ -0,0 +1,23 @@
+//http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
+function replaceURLWithHTMLLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp,"<a href='$1'>$1</a>"); 
+}
+
+$(function() {
+	$("td:contains('http')").each(function() {
+		var txt = $(this).html();
+		var replaced = replaceURLWithHTMLLinks(txt);
+		$(this).html(replaced);
+	});
+
+	$("td:contains('@')").each(function() {
+		var txt = $(this).html();
+		$(this).html(txt.replace(/@([A-Za-z0-9]+)/g, '<a href="https://twitter.com/$1">@$1</a>'));
+	});
+
+	$("td:contains('#')").each(function() {
+		var txt = $(this).html();
+		$(this).html(txt.replace(/(#[A-Za-z0-9]+)/g, '<a href="https://twitter.com/search/$1">$1</a>'));
+	});
+});
